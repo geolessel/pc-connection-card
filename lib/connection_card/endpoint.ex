876fc81b100ee23a9605d2f8fc2cf08a2646225b
Geoffrey Lessel <geo@geoffreylessel.com>
2016-08-01 21:34:24 -0700

    Start your engines

---
+++ lib/connection_card/endpoint.ex
@@ -0,0 +1,39 @@
+defmodule ConnectionCard.Endpoint do
+  use Phoenix.Endpoint, otp_app: :connection_card
+
+  socket "/socket", ConnectionCard.UserSocket
+
+  # Serve at "/" the static files from "priv/static" directory.
+  #
+  # You should set gzip to true if you are running phoenix.digest
+  # when deploying your static files in production.
+  plug Plug.Static,
+    at: "/", from: :connection_card, gzip: false,
+    only: ~w(css fonts images js favicon.ico robots.txt)
+
+  # Code reloading can be explicitly enabled under the
+  # :code_reloader configuration of your endpoint.
+  if code_reloading? do
+    socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
+    plug Phoenix.LiveReloader
+    plug Phoenix.CodeReloader
+  end
+
+  plug Plug.RequestId
+  plug Plug.Logger
+
+  plug Plug.Parsers,
+    parsers: [:urlencoded, :multipart, :json],
+    pass: ["*/*"],
+    json_decoder: Poison
+
+  plug Plug.MethodOverride
+  plug Plug.Head
+
+  plug Plug.Session,
+    store: :cookie,
+    key: "_connection_card_key",
+    signing_salt: "k0IIdpBQ"
+
+  plug ConnectionCard.Router
+end
