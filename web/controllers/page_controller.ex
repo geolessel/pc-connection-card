0ef3688abdde6ebe98a90702ec9c4e239ab9b89f
Geoffrey Lessel <geo@geoffreylessel.com>
2016-08-01 23:02:58 -0700

    Add ability to create a person through the API

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,13 +1,19 @@
 defmodule ConnectionCard.PageController do
   use ConnectionCard.Web, :controller
   import Logger
+  alias PcoApi.People.Person
 
   def index(conn, _params) do
     render conn, "index.html"
   end
 
   def signup(conn, %{"person" => person} = params) do
-    conn = put_flash(conn, :info, "#{person["first_name"]} #{person["last_name"]}")
+    %PcoApi.Record{attributes: attrs, id: id} =
+      person
+      |> Enum.into([], fn {k,v} -> {String.to_atom(k), v} end)
+      |> Person.new
+      |> Person.create
+    conn = put_flash(conn, :info, "Created #{attrs["first_name"]} #{attrs["last_name"]} (ID #{id})")
     redirect conn, to: "/"
   end
 end
